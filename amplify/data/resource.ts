f5e5c5c09e23ecf8d4f657567e0bf776d1f3f865
offlineprogrammer <mmalaka@gmail.com>
2024-09-12 20:17:29 -0700

    1st commit

---
+++ amplify/data/resource.ts
@@ -0,0 +1,31 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const SYSTEM_PROMPT =
+ 'You are a helpful assistant that generates recipes.'
+  + ' Please generate a recipe based on the following description:';
+const schema = a.schema({
+  generateRecipe: a.generation({
+    aiModel: a.ai.model('Claude 3 Haiku'),
+    systemPrompt: SYSTEM_PROMPT ,
+  })
+  .arguments({
+    description: a.string(),
+  })
+  .returns(
+    a.customType({
+      name: a.string(),
+      ingredients: a.string().array(),
+      instructions: a.string(),
+    })
+  )
+  .authorization((allow) => allow.authenticated()),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "userPool",
+  },
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,91 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { createAIHooks } from "@aws-amplify/ui-react-ai";
+import { amplifyClient } from "@/app/amplify-utils";
 
-export default function Home() {
+import {
+  Heading,
+  Loader,
+  Placeholder,
+  Text,
+  View,
+} from "@aws-amplify/ui-react";
+
+import React, { FormEvent } from "react";
+
+const { useAIGeneration } = createAIHooks(amplifyClient);
+
+export default function Page() {
+  const [{ data, isLoading}, generateRecipe] =
+    useAIGeneration("generateRecipe");
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const formData = new FormData(event.currentTarget);
+
+      generateRecipe({
+        description: formData.get("description")?.toString() || "",
+      });
+    } catch (e) {
+      alert(`An error occurred: ${e}`);
+    } finally {
+    }
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className="app-container">
+      <div className="header-container">
+        <h1 className="main-header">
+          Meet Your Personal
+          <br />
+          <span className="highlight">Recipe AI</span>
+        </h1>
+      </div>
+      <form onSubmit={onSubmit} className="form-container">
+        <div className="search-container">
+          <input
+            type="text"
+            className="wide-input"
+            id="description"
+            name="description"
+          />
+          <button type="submit" className="search-button">
+            Generate
+          </button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </form>
+      <div className="result-container">
+        {isLoading ? (
+          <div className="loader-container">
+            <p>Loading...</p>
+            <Loader size="large" />
+            <Placeholder size="large" />
+            <Placeholder size="large" />
+            <Placeholder size="large" />
+          </div>
+        ) : (
+          data?.name && (
+            <p className="result">
+              <Heading level={3}>{data?.name}</Heading>
+              <br />
+              <Heading level={4}>Ingredients:</Heading>
+              <br />
+              <View as="ul">
+                {data?.ingredients?.map((ingredient: unknown) => (
+                  <Text as="li" key={ingredient}>
+                    {ingredient}
+                  </Text>
+                ))}
+              </View>
+              <br />
+              <Heading level={4}>Instructions:</Heading>
+              <br />
+              <Text>{data?.instructions}</Text>
+            </p>
+          )
+        )}
+      </div>
     </div>
   );
 }
